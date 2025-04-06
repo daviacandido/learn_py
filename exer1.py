6920f55cb0ee6de60b7c0a4d409cb747a0eba969
daviacandido <daviaraujocandido@gmail.com>
2025-04-06 00:08:06 -0300

    exercicio finalizado

--- exer1.py
+++ exer1.py
@@ -13,3 +13,16 @@ Se nome e idade forem digitados:
 Se nada for digitado em nome ou idade:
     exiba "Desculpe, você deixou campos vazios."
 """
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+if nome and idade != "":
+    contagem_espaco = nome.count(' ')
+    print(f"Seu nome é {nome}")
+    print(f"Seu nome invertido é {nome[::-1]}")
+    print(f"Seu nome {'não contém espaço' if contagem_espaco == 0 else f'contem {contagem_espaco} espaços'}")
+    print(f"Seu nome tem {len(nome)} letras")
+    print(f"A primeira letra do seu nome é {nome[0]}")
+    print(f"A última letra do seu nome é {nome[-1]}")
+else:
+    print("Desculpe, você deixou campos vazios.")
