47d53b6b9252d46a65d8a90cc018585464e541c7
daviacandido <daviaraujocandido@gmail.com>
2025-04-17 09:28:46 -0300

    Flake8 + importOS usando clear para limpar terminal a cada execução

--- exercicios/basico/calculadora.py
+++ exercicios/basico/calculadora.py
@@ -1,53 +1,61 @@
+import os
+
 # Calculadora
 
 while True:
-    numero_1 = input('Digite o primeiro número: ')
-    numero_2 = input('Digite o segundo número: ')
-    operador = input('Digite qual o operador a ser usado (+-/*)')
+    os.system('clear')
+
+    # Variaveis
+    numero_1 = input('Insira o primeiro número: ')
+    numero_2 = input('Insira o segundo número: ')
+    operador = input('Insira um operador (+, -, *, /): ')
 
     numeros_validos = None
 
+    # Verificacao de entradas do usuário:
     try:
-        num_1_float = float(numero_1)
-        num_2_float = float(numero_2)
+        numero_1_float = float(numero_1)
+        numero_2_float = float(numero_2)
         numeros_validos = True
-    except:
+
+    except ValueError:
         numeros_validos = None
 
     if numeros_validos is None:
-        print('Um ou ambos os números digitados são inválidos.')
+        print('Um ou ambos os números inseridos são inválidos')
         continue
-
-    operadores_permitidos = '+-/*'
 
     if len(operador) == 0:
-        print('Digite um operador:')
+        print('Insira pelo menos um operador')
         continue
-    
+
     if len(operador) > 1:
-        print('Digite apenas um operador.')
+        print('Insira apenas um operador')
         continue
 
-    if operador not in operadores_permitidos:
-        print('Operador inválido.')
+    operadores_validos = '+-*/'
+
+    if operador not in operadores_validos:
+        print('Operador inválido')
         continue
 
+    # Calculos:
+    print('Realizando os calculos: ')
 
-    print('Realizando a sua conta, confira o resultado abaixo: ')
-    
     if operador == '+':
-        print(num_1_float + num_2_float)
+        print(f'{numero_1_float} + {numero_2_float} = ',
+              numero_1_float + numero_2_float)
     if operador == '-':
-        print(num_1_float - num_2_float)
+        print(f'{numero_1_float} - {numero_2_float} = ',
+              numero_1_float - numero_2_float)
     if operador == '/':
-        print(num_1_float / num_2_float)
+        print(f'{numero_1_float} / {numero_2_float} = ',
+              numero_1_float / numero_2_float)
     if operador == '*':
-        print(num_1_float * num_2_float)
-    else:
-        print('Não deveria estar aqui.')
+        print(f'{numero_1_float} * {numero_2_float} = ',
+              numero_1_float * numero_2_float)
 
-
-    sair = input('Quer sair? [s]im: ').lower().startswith('s')
-
+    # Verifica saida
+    sair = input('Deseja sair? [s]im: ').lower().startswith('s')
     if sair is True:
         break
