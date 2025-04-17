8692a6508a18afc91543bf236f114389761c8d47
daviacandido <daviaraujocandido@gmail.com>
2025-04-16 22:40:12 -0300

    iniciando exercicio de calculadora

---
+++ exercicios/basico/calculadora.py
@@ -0,0 +1,36 @@
+# Calculadora
+
+while True:
+    numero_1 = input('Digite o primeiro número: ')
+    numero_2 = input('Digite o segundo número: ')
+    operador = input('Digite qual o operador a ser usado (+-/*)')
+
+    numeros_validos = None
+
+    try:
+        num_1_float = float(numero_1)
+        num_2_float = float(numero_2)
+        numeros_validos = True
+    except:
+        numeros_validos = None
+
+    if numeros_validos is None:
+        print('Um ou ambos os números digitados são inválidos.')
+        continue
+
+    operadores_permitidos = '+-/*'
+
+    if operador not in operadores_permitidos:
+        print('Operador inválido.')
+        continue
+
+    if len(operador) > 1:
+        print('Digite apenas um operador.')
+        continue
+
+    sair = input('Quer sair? [s]im: ').lower().startswith('s')
+    print(sair)
+
+    if sair is True:
+        break
+    
