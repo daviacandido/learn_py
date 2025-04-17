b2ddc874c7a450d6799a714026559b97a3d18004
daviacandido <daviaraujocandido@gmail.com>
2025-04-17 10:23:38 -0300

    while else

---
+++ aulas/basico/whileelse.py
@@ -0,0 +1,16 @@
+# while else
+
+string = 'x'
+
+i = 0
+
+while i < len(string):
+    letra = string[i]
+
+    print(letra)
+    i += 1
+
+else:
+    print('o Else foi executado.')
+
+print('fora do else')
