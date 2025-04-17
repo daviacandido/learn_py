7acd9fd9add44e0ffe45f5c4a37961752cbe4550
daviacandido <daviaraujocandido@gmail.com>
2025-04-17 10:26:14 -0300

    novo exercicio: vendo qual letra apareceu mais vezes na frase

---
+++ exercicios/basico/itstringscomwhile.py
@@ -0,0 +1,7 @@
+# Qual letra apareceu mais vezes na frase?
+
+frase = 'O python é uma linguagem de programação '\
+    'multiparadigma. ' \
+    'Python foi criado por Guido van Rossum.'
+
+print(frase)
