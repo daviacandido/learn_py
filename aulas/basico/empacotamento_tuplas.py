ba371613b3aa4b135cdd4ce1838fd9c436993a9c
daviacandido <daviaraujocandido@gmail.com>
2025-05-07 23:35:50 -0300

    empacotamento e tuplas

---
+++ aulas/basico/empacotamento_tuplas.py
@@ -0,0 +1,12 @@
+# introdução a empacotamento + tuplas:
+
+nomes = ['Maria', 'João', 'Luiz']
+nome1, nome2, nome3 = nomes
+
+print(f'Lista:{nomes};\nNome 1: {nome1}\nNome 2: {nome2}\nNome 3: {nome3}')
+
+nomes.append('Davi')
+
+_, _, _, davi, *_ = nomes
+
+print(f'Novo nome: {davi}')
