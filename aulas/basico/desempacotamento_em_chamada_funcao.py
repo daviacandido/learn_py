b3997c5d43e82fed8f65c2abfd7f607e23a6e5a6
daviacandido <daviaraujocandido@gmail.com>
2025-05-14 16:23:39 -0300

    desempacotamento em chamada de funcao

---
+++ aulas/basico/desempacotamento_em_chamada_funcao.py
@@ -0,0 +1,11 @@
+lista = ['Maria', 'Helena', 'Eduarda']
+salas = [
+    ['Maria', 'Helena'],
+    ['Claudio', 'Claudia', 'Anderson'],
+    ['Luiz', 'Davi']
+]
+
+print(*salas, sep='\n')
+print(*lista, end='\n', sep=', ')
+
+
