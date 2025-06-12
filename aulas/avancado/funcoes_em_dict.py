47510f8e7322d78ba7a7be107389b10726a111b3
daviacandido <daviaraujocandido@gmail.com>
2025-06-12 09:27:15 -0300

    funcoes em dicionarios

--- aulas/avancado/funcoes_em_dict.py
+++ aulas/avancado/funcoes_em_dict.py
@@ -9,11 +9,14 @@
 # pop - Apaga um item com a chave especificada (del)
 # popitem - Apaga o último item adicionado
 # update - Atualiza um dicionário com outro
+
 pessoa = {
     'nome': 'Davi',
     'sobrenome': 'Cândido',
     # 'idade': 0,
 }
+
+# len, keys, values, items e suas funcionalidades:
 
 print(len(pessoa))
 print(list(pessoa.keys()))
@@ -27,3 +30,59 @@ for valor in pessoa.values():
 
 for chave, valor in pessoa.items():
     print(f'{chave}: {valor}')
+
+
+# Diferença entre deep e shallow copy:
+
+d1 = {
+    'c1': 1,
+    'c2': 2
+}
+
+d2 = {
+    'c1': 100,
+    'c2': 200
+}
+
+deep_copy_d1 = d1
+
+print(deep_copy_d1)
+d1['c1'] = 10
+print(deep_copy_d1)
+
+shallow_copy_d2 = d2.copy()
+
+print(shallow_copy_d2)
+d2['c1'] = 10
+print(shallow_copy_d2)
+
+# dict.pop() e dict.get()
+
+print('pessoa.pop("idade")')
+pessoa.pop('idade')
+print(pessoa.get('idade', 'Não existe a chave selecionada'))
+
+# dict.popitem()
+
+print(d2)
+print('d2.popitem()')
+d2.popitem()
+print(d2)
+
+# dict.update()
+
+pessoa.update({
+    'nome do meio': 'Araújo',
+    'idade': 18
+})
+
+# ou
+
+pessoa.update(teste='teste')
+
+# ou
+
+tupla_update = ('chave', 'valor'),
+pessoa.update(tupla_update)
+
+print(pessoa)
