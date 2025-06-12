9cef463dd6c2dc791f9f41592785463f4d6a2b60
daviacandido <daviaraujocandido@gmail.com>
2025-06-12 10:54:03 -0300

    exercicio com dict finalizado

--- exercicios/avancado/dict.py
+++ exercicios/avancado/dict.py
@@ -17,3 +17,46 @@ perguntas = [
         'Resposta': '5',
     },
 ]
+
+
+def verifica_acerto(escolha, resposta, lista_opcoes):
+    qtd_opcoes = len(lista_opcoes)
+    if escolha is not None:
+        if escolha >= 0 and escolha < qtd_opcoes:
+            if lista_opcoes[escolha] == resposta:
+                return True
+
+
+acertos = 0
+
+for pergunta in perguntas:
+
+    print(f'Pergunta: {pergunta["Pergunta"]}')
+    print()
+
+    opcoes = pergunta['Opções']
+
+    for i, opcao in enumerate(opcoes):
+        print(f'{i}) {opcao}')
+    print()
+
+    escolha = None
+    while escolha is None:
+        try:
+            escolha_input = input(f'Escolha a resposta entre 0 e {len(opcoes) - 1}: ')
+            escolha = int(escolha_input)
+            if escolha < 0 or escolha >= len(opcoes):
+                print(f'Escolha um valor válido entre 0 e {len(opcoes) - 1}')
+                escolha = None
+        except ValueError:
+            print(f'Entrada inválida! Escolha um valor inteiro entre 0 e {len(opcoes) - 1}')
+    qtd_opcoes = len(opcoes)
+
+    if verifica_acerto(escolha, pergunta['Resposta'], opcoes):
+        acertos += 1
+        print('Acertou!')
+    else:
+        print('Errou!')
+    print()
+
+print(f'Você acertou {acertos} de {len(perguntas)} questões')
