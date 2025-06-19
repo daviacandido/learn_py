c4e7d7a71eb9e325979c7de2564aeced3744ca64
Davi Candido <daviaraujocandido@gmail.com>
2025-06-19 02:28:41 +0000

    mapeamento em list comprehension

--- aulas/avancado/list_comprehension.py
+++ aulas/avancado/list_comprehension.py
@@ -1,4 +1,25 @@
+# lista comum:
+
+lista = []
+for numero in range(10):
+    lista.append(numero)
+print(lista)
+# list comprehension é uma forma
 # criar lista a partir de iteraveis:
 
 lista = [numero * 2 for numero in range(10)]
 print(lista)
+
+# mapeamento em list comprehension:
+
+produtos = [
+    {'nome': 'p1', 'preco': 20, },
+    {'nome': 'p2', 'preco': 30},
+]
+
+novos_produtos = [
+    produto
+    for produto in produtos
+]
+
+print(novos_produtos)
