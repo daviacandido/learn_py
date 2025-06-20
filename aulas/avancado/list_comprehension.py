7147c419fc2f67a1a95959a820d44571962effde
daviacandido <daviaraujocandido@gmail.com>
2025-06-20 11:25:01 -0300

    pprint

--- aulas/avancado/list_comprehension.py
+++ aulas/avancado/list_comprehension.py
@@ -1,26 +1,39 @@
+import pprint
+
+
+def p(variavel):
+    pprint.pprint(novos_produtos, sort_dicts=False, width=40)
+
+
 # lista comum:
 
 lista = []
 for numero in range(10):
     lista.append(numero)
-print(lista)
+# print(lista)
+
 # list comprehension é uma forma
 # criar lista a partir de iteraveis:
 
 lista = [numero * 2 for numero in range(10)]
-print(lista)
+# print(lista)
 
 # mapeamento em list comprehension:
 
 produtos = [
-    {'nome': 'p1', 'preco': 20},
-    {'nome': 'p2', 'preco': 30},
+    {'nome': 'arroz', 'preco': 10},
+    {'nome': 'feijao', 'preco': 20},
+    {'nome': 'carne', 'preco': 30}
 ]
 
 novos_produtos = [
+    # mapeamento:
     {**produto, 'preco': produto['preco'] * 1.05}
     if produto['preco'] > 20 else {**produto}
     for produto in produtos
+    # filtro:
+
+
 ]
 
-print(*novos_produtos, sep='\n')
+p(novos_produtos)
