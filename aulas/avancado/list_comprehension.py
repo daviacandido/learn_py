e801a602c8c0739f181d1338e27ee4087e763f93
daviacandido <daviaraujocandido@gmail.com>
2025-06-20 10:12:47 -0300

    mapeamento em list comprehension

--- aulas/avancado/list_comprehension.py
+++ aulas/avancado/list_comprehension.py
@@ -13,13 +13,14 @@ print(lista)
 # mapeamento em list comprehension:
 
 produtos = [
-    {'nome': 'p1', 'preco': 20, },
+    {'nome': 'p1', 'preco': 20},
     {'nome': 'p2', 'preco': 30},
 ]
 
 novos_produtos = [
-    produto
+    {**produto, 'preco': produto['preco'] * 1.05}
+    if produto['preco'] > 20 else {**produto}
     for produto in produtos
 ]
 
-print(novos_produtos)
+print(*novos_produtos, sep='\n')
